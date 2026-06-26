a5427d673c40493f5c443cb49034077653e5b18f
judeibor <judeibor234@gmail.com>
2026-06-26 08:32:34 -0400

    Add ban command for banning users in Discord

---
+++ discord-bot/commands/ban.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ban')
+    .setDescription('Ban a member.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('User to ban')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName('reason')
+        .setDescription('Reason for the ban')
+        .setRequired(false)
+    ),
+  async execute(interaction) {
+    const user = interaction.options.getUser('user');
+    const reason = interaction.options.getString('reason') || 'No reason provided';
+    const member = await interaction.guild.members.fetch(user.id).catch(() => null);
+
+    if (!member) {
+      return interaction.reply({ content: 'That user is not in this server.', ephemeral: true });
+    }
+
+    await member.ban({ reason });
+    await interaction.reply({ content: `Banned ${user.tag}. Reason: ${reason}`, ephemeral: true });
+  }
+};

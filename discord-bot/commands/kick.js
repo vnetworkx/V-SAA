0f4d72599af1c8a498f83b923cdfc2fa8b8ac0b6
judeibor <judeibor234@gmail.com>
2026-06-26 08:33:57 -0400

    Add kick command for Discord bot

---
+++ discord-bot/commands/kick.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('kick')
+    .setDescription('Kick a member.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.KickMembers)
+    .addUserOption(option =>
+      option.setName('user')
+        .setDescription('User to kick')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName('reason')
+        .setDescription('Reason for the kick')
+        .setRequired(false)
+    ),
+  async execute(interaction) {
+    const user = interaction.options.getUser('user');
+    const reason = interaction.options.getString('reason') || 'No reason provided';
+    const member = await interaction.guild.members.fetch(user.id).catch(() => null);
+
+    if (!member) {
+      return interaction.reply({ content: 'That user is not in this server.', ephemeral: true });
+    }
+
+    await member.kick(reason);
+    await interaction.reply({ content: `Kicked ${user.tag}. Reason: ${reason}`, ephemeral: true });
+  }
+};

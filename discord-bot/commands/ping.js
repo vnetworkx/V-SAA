7c5cdc999ed1152c2b385a87a5b6ce7f1155cbde
judeibor <judeibor234@gmail.com>
2026-06-26 08:38:51 -0400

    Update ping command response and description

--- discord-bot/commands/ping.js
+++ discord-bot/commands/ping.js
@@ -1,11 +1,10 @@
-// commands/ping.js
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Replies with Pong!'),
+    .setDescription('Replies with Pong.'),
   async execute(interaction) {
-    await interaction.reply('Pong!');
-  },
+    await interaction.reply(`Pong! ${interaction.client.ws.ping}ms`);
+  }
 };

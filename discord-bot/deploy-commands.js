ce2b638ccae01f6965594f8674f142e7791b41f3
judeibor <judeibor234@gmail.com>
2026-06-26 08:20:27 -0400

    Add deploy-commands.js for deploying slash commands

---
+++ discord-bot/deploy-commands.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const fs = require('node:fs');
+const path = require('node:path');
+const { REST, Routes } = require('discord.js');
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  if ('data' in command && 'toJSON' in command.data) {
+    commands.push(command.data.toJSON());
+  }
+}
+
+const rest = new REST().setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    console.log(`Deploying ${commands.length} slash commands...`);
+
+    await rest.put(
+      Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+      { body: commands }
+    );
+
+    console.log('Slash commands deployed successfully.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
